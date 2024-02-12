9dbdcd93416c9f9b1545063e21052150e3fd1414
RS <RS@LAPTOP-VCPGANQJ>
2024-02-12 13:24:55 -0600

    Ejercicio 1

--- index.js
+++ index.js
@@ -9,9 +9,30 @@ let lentes = [
     {id: 2, name: "steampunk"},
     {id: 3, name: "retro"}
 ];
-
-app.listen(port,()=>{console.log("Server is connected")});
-
-app.get('/',(req,res)=>{
-    res.send("Hola mundo");
+app.get("/lentes", (req, res) =>{
+    res.send(lentes)
 });
+
+app.post("/lentes", (req, res) => {
+    const { body }  = req
+    games.push(body)
+    res.send("Se agrego el lente")
+});
+
+app.patch("/lentes/:id", (req, res) => {
+    const { body }  = req
+    const { name } = body
+    const { id } = req.params;
+    
+    let lente = lentes.find((lente) => lente.id == id)
+    lente.name = name
+    res.send({message: "se elimino el lente", lente})
+});
+
+app.delete("/lentes/:id", (req, res) => {
+    const { id } = req.params;
+    lentes = lentes.filter((lente) => lente.id != id)
+    res.send("Se elimino el lente")
+});
+
+app.listen(3000)
